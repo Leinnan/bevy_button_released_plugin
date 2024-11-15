a57f90747fa037c86b8813bf0403462256c170fd
Leinnan <siuszko@zoho.com>
2024-11-15 16:17:37 +0000

    Build: (bf44728) Observer way of thinking

---
+++ simple_bg.wasm.d.ts
@@ -0,0 +1,29 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(a: number, b: number): number;
+export function wgpu_render_bundle_set_pipeline(a: number, b: number): void;
+export function wgpu_render_bundle_set_bind_group(a: number, b: number, c: number, d: number, e: number): void;
+export function wgpu_render_bundle_set_vertex_buffer(a: number, b: number, c: number, d: number, e: number): void;
+export function wgpu_render_bundle_set_push_constants(a: number, b: number, c: number, d: number, e: number): void;
+export function wgpu_render_bundle_draw(a: number, b: number, c: number, d: number, e: number): void;
+export function wgpu_render_bundle_draw_indexed(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function wgpu_render_bundle_draw_indirect(a: number, b: number, c: number): void;
+export function wgpu_render_bundle_draw_indexed_indirect(a: number, b: number, c: number): void;
+export function wgpu_render_bundle_set_index_buffer(a: number, b: number, c: number, d: number, e: number): void;
+export function wgpu_render_bundle_pop_debug_group(a: number): void;
+export function wgpu_render_bundle_insert_debug_marker(a: number, b: number): void;
+export function wgpu_render_bundle_push_debug_group(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_export_3: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hca7f55454c3d8ddf(a: number, b: number): void;
+export function closure1364_externref_shim(a: number, b: number, c: number): void;
+export function closure1367_externref_shim(a: number, b: number, c: number, d: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h68be45a4dcfe954d(a: number, b: number): void;
+export function closure70880_externref_shim(a: number, b: number, c: number): void;
+export function __externref_table_alloc(): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
